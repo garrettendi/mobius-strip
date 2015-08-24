a55dc5a50b0dc93d50e6a3853771f701f7907fb8
garrettendi <dale.hards@googlemail.com>
2015-08-24 16:24:51 +0100

    Rewrite to use Win Forms and own curses-like code

--- mobius-strip/mobius-strip/Main.cs
+++ mobius-strip/mobius-strip/Main.cs
@@ -1,187 +1,25 @@
-using CursesSharp;
 using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Mobius
 {
   class MainClass
   {  
     /// <summary>
-    /// Width of party window. Constant.
-    /// </summary>
-    public const int PARTY_WIDTH = 14;
-    
-    /// <summary>
-    /// Height of journal window. Constant.
-    /// </summary>
-    public const int JOURNAL_HEIGHT = 7;
-    
-    /// <summary>
     /// The entry point of the program, where the program control starts and ends.
     /// </summary>
     /// <param name='args'>
     /// The command-line arguments. Do not specify any.
     /// </param>
-    public static void Main (string[] args)
+    [STAThread]
+    static void Main()
     {
-      Curses.InitScr();
-      try
-      {
-        MainClass m = new MainClass();
-        
-        m.Main2(args);
-      }
-      finally
-      {
-        Curses.EndWin();
-      }
-    }
-    
-    /// <summary>
-    /// Used to enter execution.
-    /// </summary>
-    /// <param name='args'>
-    /// Arguments. Follow through from Main execution.
-    /// </param>
-    public void Main2 (string[] args)
-    {
-      Stdscr.Blocking = false;
-      Curses.Echo = false;
-      
-      Stdscr.Erase();
-      
-      DrawStatsBar();
-      
-      DrawMapWin();
-      
-      DrawPartyWin();
-      
-      DrawJournalWin();
-      
-      RefreshScreen();     
-      
-      while(true)
-        continue;
-    }
-       
-    /// <summary>
-    /// Taken from the FireworkDemo of CursesSharp - GNU LESSER GENERAL PUBLIC LICENSE 
-    /// http://curses-sharp.sourceforge.net/ - Original Author: R. Konklewski. 
-    /// Used to check and add strings anywhere in the terminal.
-    /// </summary>
-    /// <param name='x'>
-    /// Column to insert string at.
-    /// </param>
-    /// <param name='y'>
-    /// Line to add string at.
-    /// </param>
-    /// <param name='str'>
-    /// String to add.
-    /// </param>
-    public static void CheckAddString(int x, int y, string str)
-    {
-      if (x >= 0 && x < Curses.Cols && y >= 0 && y < Curses.Lines)
-        Stdscr.Add(y, x, str);
-    }
-    
-    /// <summary>
-    /// Creates a full line of text
-    /// </summary>
-    /// <param name='prePend'>
-    /// Text to place at the beginning of the line
-    /// </param>
-    /// <param name='postPend'>
-    /// Text to place at the end of the line.
-    /// </param>
-    /// <param name='filler'>
-    /// Filler character to fill the rest of the line - this can be the empty space.
-    /// </param>
-    /// <param name='y'>
-    /// The line to place this on
-    /// </param>
-    /// <param name='preXOffset'>
-    /// Which column to place the start of the line at
-    /// </param>
-    /// <param name='postXOffset'>
-    /// Columns away from the end of a full terminal line to end at.
-    /// </param>
-    public static void FullLine(string prePend, string postPend, char filler, int y, int preXOffset, int postXOffset)
-    {
-      if (y < 0 || y >= Curses.Lines)
-        return;
-      StringBuilder str = new StringBuilder();
-      str.Append(prePend);
-      
-      int fillLength = Curses.Cols - prePend.Length - postPend.Length - preXOffset - postXOffset + 1;
-      
-      for (int i = 1; i < fillLength; i++)
-        str.Append(filler);
-      
-      str.Append(postPend);
-      
-      // Remove last char for blinking cursor
-      if (y == Curses.Lines - 1)
-        str.Remove(str.Length - 1, 1);
-      
-      Stdscr.Add(y, preXOffset, str.ToString());
-    }
-    
-    /// <summary>
-    /// Refreshes the screen and moves cursor to bottom right of terminal.
-    /// </summary>
-    public static void RefreshScreen()
-    {
-      Stdscr.Move(Curses.Lines - 1, Curses.Cols - 1);
-      Stdscr.Refresh();
-    }
-  
-    /// <summary>
-    /// Draws the stats bar.
-    /// </summary>
-    private static void DrawStatsBar()
-    {
-      FullLine(" STATS (1) ::  STR:100  MAG:100  HIT:100  ", "", ' ', 0, 0, 0);
-    }
-  
-    /// <summary>
-    /// Draws the map window.
-    /// </summary>
-    private static void DrawMapWin()
-    {
-      FullLine("*-Map", "*", '-', 1, 0, PARTY_WIDTH + 1);
-      
-      for (int i = 2; i < Curses.Lines - JOURNAL_HEIGHT - 1; i++)
-        FullLine("|", "|", ' ', i, 0, PARTY_WIDTH + 1);
-      
-      FullLine("*", "*", '-', Curses.Lines - JOURNAL_HEIGHT - 1, 0, PARTY_WIDTH + 1);
-    }
-    
-    /// <summary>
-    /// Draws the party window.
-    /// </summary>
-    private static void DrawPartyWin()
-    {
-      FullLine("*-Party", "*", '-', 1, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("|", "|", ' ', 2, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("| (1)", "|", ' ', 3, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("| STR: 100", "|", ' ', 4, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("|", "|", ' ', 5, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("| (2)", "|", ' ', 6, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("| STR: 100", "|", ' ', 7, Curses.Cols - PARTY_WIDTH, 0);
-      for (int i = 8; i < Curses.Lines - JOURNAL_HEIGHT - 1; i++)
-        FullLine("|", "|", ' ', i, Curses.Cols - PARTY_WIDTH, 0);
-      FullLine("*", "*", '-', Curses.Lines - JOURNAL_HEIGHT - 1, Curses.Cols - PARTY_WIDTH, 0);
-    }
-  
-    /// <summary>
-    /// Draws the journal window.
-    /// </summary>
-    private static void DrawJournalWin()
-    {
-      FullLine("*-Journal", "*", '-', Curses.Lines - JOURNAL_HEIGHT, 0, 0);
-      for (int i = Curses.Lines - JOURNAL_HEIGHT + 1; i <= Curses.Lines - 1; i++)
-        FullLine("|", "|", ' ', i, 0, 0);     
-      FullLine("*", "*", '-', Curses.Lines - 1, 0, 0);
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
     }
   }
 }
---
+++ mobius-strip/mobius-strip/MainForm.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mobius
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+
+      this.Resize += Form1_Resize;
+
+      this.Load += Form1_Load;
+    }
+
+    void Form1_Load(object sender, EventArgs e)
+    {
+      this.OnResize();
+      this.EmptyScreen();
+      this.RedrawScreen();
+    }
+
+    private int width = 0;
+    public int ConsoleWidth
+    {
+      get
+      {
+        return width;
+      }
+    }
+    private int height = 0;
+    public int ConsoleHeight
+    {
+      get
+      { 
+        return height; 
+      }
+    }
+
+    public void SetDisplay(StringBuilder display)
+    {
+      //for (int i = 1; i < this.ConsoleHeight; i++)
+      //{
+      //  display.Insert(i * (ConsoleHeight - 1), "\r\n");
+      //}
+      this.textBox1.Text = display.ToString();
+    }
+
+    public void OnResize()
+    {
+      Graphics g = this.CreateGraphics();
+      int fw = (int)g.MeasureString("H", textBox1.Font).Width;
+      g.Dispose();
+
+      decimal result = this.Width * (decimal)1.45 / fw;
+      this.width = (int)Math.Floor(result);
+
+      result = this.Height / (textBox1.Font.Height);
+      this.height = (int)Math.Floor(result);
+
+      this.RedrawScreen();
+    }
+
+    void Form1_Resize(object sender, EventArgs e)
+    {
+      OnResize();
+    }
+
+    /// <summary>
+    /// Width of party window. Constant.
+    /// </summary>
+    public const int PARTY_WIDTH = 14;
+
+    /// <summary>
+    /// Height of journal window. Constant.
+    /// </summary>
+    public const int JOURNAL_HEIGHT = 7;
+
+    /// <summary>
+    /// Used to enter execution.
+    /// </summary>
+    /// <param name='args'>
+    /// Arguments. Follow through from Main execution.
+    /// </param>
+    public void RedrawScreen()
+    {
+      this.EmptyScreen();
+
+      DrawStatsBar();
+
+      DrawMapWin();
+
+      DrawPartyWin();
+
+      DrawJournalWin();
+
+      //RefreshScreen();
+
+      this.textBox1.Select(0, 0);
+    }
+
+    private void DrawMapWin()
+    {
+      AddFullLine("*", "*", "-", this.ConsoleWidth - PARTY_WIDTH, 0, 1);
+      for (int i = 2; i < this.ConsoleHeight - JOURNAL_HEIGHT - 1; i++)
+        AddFullLine("|", "|", " ", this.ConsoleWidth - PARTY_WIDTH, 0, i);
+      AddFullLine("*", "*", "-", this.ConsoleWidth - PARTY_WIDTH, 0, this.ConsoleHeight - JOURNAL_HEIGHT - 1);
+    }
+
+    public void EmptyScreen()
+    {
+      StringBuilder builder = new StringBuilder();
+      for (int i = 0; i < this.ConsoleWidth; i++)
+      {
+        builder.Append("£");
+      }
+      for(int i = 1; i < this.ConsoleHeight; i++)
+      {
+        builder.Append("\r\n");
+        for (int j = 0; j < this.ConsoleWidth; j++)
+        {
+          builder.Append("£");
+        }
+      }
+      this.SetDisplay(builder);
+    }
+
+    /// <summary>
+    /// Taken from the FireworkDemo of CursesSharp - GNU LESSER GENERAL PUBLIC LICENSE 
+    /// http://curses-sharp.sourceforge.net/ - Original Author: R. Konklewski. 
+    /// Used to check and add strings anywhere in the terminal.
+    /// </summary>
+    /// <param name='x'>
+    /// Column to insert string at.
+    /// </param>
+    /// <param name='y'>
+    /// Line to add string at.
+    /// </param>
+    /// <param name='str'>
+    /// String to add.
+    /// </param>
+    public void CheckAddString(int col, int line, string str)
+    {
+      if (col >= 0 && col < this.ConsoleWidth && line >= 0 && line < this.ConsoleHeight)
+      {
+        string raw = this.textBox1.Text;
+
+        int index = line * (this.ConsoleWidth + 2) + col;
+
+        StringBuilder builder = new StringBuilder(raw);
+        IEnumerable<char> chars = str.ToCharArray().AsEnumerable();
+        int counter = 0;
+        foreach (char c in chars)
+        {
+          if (index != 0 && ((line > 0 && index % this.ConsoleWidth + 2 == 0) || (line == 0 && index % ConsoleWidth == 0)))
+            break;
+          builder[index++] = c;
+        }
+        
+        this.SetDisplay(builder);
+      }
+    }
+
+    /// <summary>
+    /// Draws the stats bar.
+    /// </summary>
+    private void DrawStatsBar()
+    {
+      AddFullLine(" STATS (1) ::  STR:100  MAG:100  HIT:100  ", "", " ", ConsoleWidth, 0, 0);
+      //AddFullLine("123456789012345678901234567890123456789012", "", " ", 42, 0, 0);
+    }
+
+    private void AddFullLine(string pre, string post, string filler, int width, int col, int line)
+    {
+      StringBuilder builder = new StringBuilder(pre);
+      while (builder.Length <= width - post.Length - 1)
+      {
+        builder.Append(filler);
+      }
+      builder.Append(post);
+
+      CheckAddString(col, line, builder.ToString());
+    }
+       
+    /// <summary>
+    /// Draws the party window.
+    /// </summary>
+    private void DrawPartyWin()
+    {
+      AddFullLine("*-Party", "*", "-", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 1);
+      AddFullLine("|", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 2);
+      AddFullLine("| (1)", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 3);
+      AddFullLine("| STR: 100", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 4);
+      AddFullLine("|", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 5);
+      AddFullLine("| (2)", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 6);
+      AddFullLine("| STR: 100", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, 7);
+      for (int i = 8; i < this.ConsoleHeight - JOURNAL_HEIGHT - 1; i++)
+        AddFullLine("|", "|", " ", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, i);
+      AddFullLine("*", "*", "-", PARTY_WIDTH, this.ConsoleWidth - PARTY_WIDTH, this.ConsoleHeight - JOURNAL_HEIGHT - 1);
+    }
+
+    /// <summary>
+    /// Draws the journal window.
+    /// </summary>
+    private void DrawJournalWin()
+    {
+      AddFullLine("*-Journal", "*", "-", this.ConsoleWidth, 0, this.ConsoleHeight - JOURNAL_HEIGHT);
+      for (int i = this.ConsoleHeight - JOURNAL_HEIGHT + 1; i < this.ConsoleHeight - 1; i++)
+        AddFullLine("|", "|", " ", this.ConsoleWidth, 0, i);
+      AddFullLine("*", "*", "-", this.ConsoleWidth, 0, this.ConsoleHeight - 1);
+    }
+
+  }
+}
